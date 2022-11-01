05615ec1574b1a0d63631b12840a9999d25b1fe0
Brandon Kreiser <bkreiser5@gmail.com>
2022-11-01 01:04:51 -0400

    initial commit

---
+++ run.py
@@ -0,0 +1,79 @@
+import graphviz
+import csv
+import os
+
+DATA_CSV = 'data.csv'
+
+brother_dict = {}
+brother_list = []
+founding_bros = []
+family_colors = ['gold', 'green', 'blue', 'red', 'orange', "purple", 'white', 'grey', 'navy', 'teal', 'pink']
+
+# generate brother dictonary
+with open(DATA_CSV, mode ='r')as file:
+    # reading the CSV file
+    csvFile = csv.reader(file)
+    index = 0
+    # displaying the contents of the CSV file
+    for lines in csvFile:
+        # read first line
+        if index == 0:
+            lines[0] = lines[0][1:]# gets rid of char \ufeff
+            founding_bros = lines
+        else:
+
+            if lines != []:
+            # checks for dupes
+                if lines[0] in brother_dict:
+                    brother_dict[lines[0]] = brother_dict[lines[0]] + [lines[1]]
+                else:
+                    brother_dict[lines[0]] = [lines[1]]
+                brother_list += [lines[0], lines[1]]
+        index += 1
+
+# remove dupes from bro list
+tmp_list = []
+for bro in brother_list:
+    if bro not in tmp_list:
+        tmp_list += [bro]
+brother_list = tmp_list
+
+
+# creates the main graph
+graph_main = graphviz.Digraph(
+    engine='dot',
+    node_attr={"shape": "box"},
+    edge_attr={"arrowhead": "none"}
+)
+
+subgraphs = []
+current_subgraph = graphviz.Digraph(
+        node_attr={"style": "filled","fillcolor": family_colors[0]}
+)
+
+index = 1
+for bro in brother_list:
+    if bro in founding_bros:
+        subgraphs.append(current_subgraph)
+        current_subgraph = graphviz.Digraph(
+            node_attr={"style": "filled","fillcolor": family_colors[index]}
+        )
+        index+=1
+    current_subgraph.node(bro, bro)
+
+index = 0
+for big in brother_dict:
+    if big in founding_bros:
+        current_subgraph = subgraphs[index]
+        index+=1
+    for little in brother_dict[big]:
+        current_subgraph.edge(big, little)
+
+for subgraph in subgraphs:
+    graph_main.subgraph(subgraph)
+
+graph_main.render('brothers', view=True)
+try:
+    os.remove(os.path.dirname(os.path.abspath(__file__)) + '/brothers')
+except:
+    print('Could not clean up after myself')
