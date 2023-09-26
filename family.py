20df8a1dbcfae30fc434fb98c92558dcd6dd6f19
Brandon Kreiser <bkreiser5@gmail.com>
2023-09-26 12:00:29 -0400

    added family object for organization

---
+++ family.py
@@ -0,0 +1,11 @@
+from brother import Brother
+
+class Family(object):
+    name = ""
+    head = Brother()
+    brothers = []
+
+    def __init__(self, h, b):
+        self.name = h.name
+        self.head = h
+        self.brother = b
