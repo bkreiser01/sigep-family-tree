421695ce3bbb411cab7ea1b1934d63e6f1985063
Brandon Kreiser <bkreiser5@gmail.com>
2023-09-26 12:27:32 -0400

    removed legacy code

--- generate_graph.py
+++ generate_graph.py
@@ -40,34 +40,6 @@ def gen_graph(fraternity):
     
     fraternity_tree.render('brothers', view=True)
     os.remove(os.path.dirname(os.path.abspath(__file__)) + '/brothers')
-    
-    # ===========LEGACY CODE===========
-    # # creates the first subgraph
-    # subgraphs = []
-    # current_subgraph = graphviz.Digraph(
-    #         node_attr={"style": "filled","fillcolor": 'Red'}
-    # )
-
-    # # create a subgraph for each family and nodes for each bro
-    # index = 1
-    # for bro in brother_list:
-    #     if bro in founding_bros:
-    #         subgraphs.append(current_subgraph)
-    #         current_subgraph = graphviz.Digraph(
-    #             node_attr={"style": "filled","fillcolor": family_colors[index]}
-    #         )
-    #         index+=1
-    #     current_subgraph.node(bro, bro)
-
-    # # iterate through big-little pairs and connect them in their subgraphs
-    # index = 0
-    # for big in brother_dict:
-    #     if big in founding_bros:
-    #         current_subgraph = subgraphs[index]
-    #         index+=1
-    #     for little in brother_dict[big]:
-    #         current_subgraph.edge(big, little)
-
 
 def main():
     gen_graph(Fraternity(DATA_CSV))
